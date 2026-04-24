bda0af2e12362c373538c91e6f60cbd4f4c43c55
JaeMinHW <bosik99@naver.com>
2026-04-24 17:07:21 +0900

    로그인 수정 이전 상태로 롤백

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_VERSION: str = "v2"
+    APP_VERSION: str = "v1"
     DATABASE_URL: str = "mysql+pymysql://root:root1234@db:3306/mengnyang"
 
     class Config:
